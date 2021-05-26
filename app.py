be40913a9813c5dbea529de192fc4d60e587acf8
mikhail matyukhin <79624065+matyuhin@users.noreply.github.com>
2021-05-26 20:26:22 +0500

    Update app.py

--- app.py
+++ app.py
@@ -6,8 +6,9 @@ from sqlalchemy.ext.declarative import declarative_base
 from flask_jwt_extended import JWTManager, jwt_required, get_jwt_identity
 import pymysql
 from config import Config
-from hashids import Hashids
 from flask_cors import CORS
+import hashlib
+import base64
 
 
 app = Flask(__name__)
@@ -33,8 +34,6 @@ cors = CORS(app, resources={
     r"/*": {"origins": Config.CORS_ALLOWED_ORIGINS}
 })
 
-hashids = Hashids(min_length=12, salt=Config.salt)
-
 
 
 
@@ -59,30 +58,51 @@ def login():
     params = request.json
     user = User.authenticate(**params)
     token = user.get_token()
-    user_id = user.id
-    return {'access_token': token, 'user_id': user_id}
+    return {'access_token': token, 'name': user.name}
 
+
+def add_original_link(original_link):
+    links = Original.query.filter(Original.link ==original_link).first()
+    if not links:
+        new_original_link = {'link': original_link}
+        add_new_original_link = Original(**new_original_link)
+        session.add(add_new_original_link)
+        session.commit()
+        links = Original.query.filter(Original.link == original_link).first()
+    return links
 
 @app.route('/add-link', methods=['POST'])
 @jwt_required()
 def add_link():
     user_id = get_jwt_identity()
-    new_one = Link(**request.json)
-    new_one.user_id = user_id
-    new_one.counter = 0
-    session.add(new_one)
-    session.commit()
-    if new_one.friendly_link:
-        friendly = new_one.friendly_link
+    response_data = request.json
+    original_link = add_original_link(response_data["original"])
+    exist = Link.query.filter(Link.id == original_link.id).first()
+    if not exist:
+        response_data.pop("original")
+        response_data["original_id"] = original_link.id
+        new_one = Link(**response_data)
+        new_one.user_id = user_id
+
+        new_one.short_link = base64.urlsafe_b64encode(hashlib.md5(original_link.link.encode("UTF-8")).digest()).decode()[:12]
+
+        new_one.counter = 0
+        session.add(new_one)
+        session.commit()
+        serialized = {
+            'original': original_link.link,
+            'short_link': f"{Config.api_url}/{new_one.short_link}",
+            'type_id': new_one.type_id,
+            'counter': new_one.counter,
+            "user_id": user_id
+        }
+        if new_one.friendly_link:
+            serialized["friendly"] = new_one.friendly_link
     else:
-        friendly = ""
-    serialized = {
-        'original': new_one.original,
-        'type_id': new_one.type_id,
-        'friendly': friendly,
-        'counter': new_one.counter,
-        "user_id": user_id
-    }
+        serialized = {
+            'message': "Запись уже существует"
+        }
+
     return jsonify(serialized)
 
 
@@ -93,17 +113,17 @@ def get_list():
     links = Link.query.filter(Link.user_id == user_id)
     serialized = []
     for link in links:
+        data = {
+            'id': link.id,
+            'original': link.original.link,
+            'short': f"{Config.api_url}/{link.short_link}",
+            'counter': link.counter,
+            'type_id': link.type_id
+        }
         if link.friendly_link:
             friendly_link = f"{Config.api_url}/{link.friendly_link}"
-        else:
-            friendly_link = ''
-        serialized.append({
-            'id': link.id,
-            'original': link.original,
-            'friendly': friendly_link,
-            'short': f"{Config.api_url}/{hashids.encode(link.id)}",
-            'counter': link.counter,
-        })
+            data['friendly'] = friendly_link
+        serialized.append(data)
     return jsonify(serialized)
 
 
@@ -139,33 +159,29 @@ def delete_link(link_id):
 
 @app.route('/<short_uri>')
 def url_redirect(short_uri):
-    link_id = hashids.decode(short_uri)
-    if link_id:
-        type_query = "id"
-        link = Link.get_original_link(*link_id, type_query)
-    else:
-        type_query = "fl"
-        link = Link.get_original_link(short_uri, type_query)
+    link = Link.query.filter(Link.short_link == short_uri).first()
+    # print(link.original.link)
+    if not link:
+        link = Link.query.filter(Link.friendly_link == short_uri).first()
     if link:
-        print(link)
-        original_link = link.original
         if link.type_id == 1:
             link.counter += 1
             session.commit()
-            return redirect(original_link)
+            return redirect(link.original.link)
         elif link.type_id == 2:
             if check_autentificate():
                 link.counter += 1
                 session.commit()
-                return redirect(original_link)
+                return redirect(link.original.link)
         else:
             if check_private_link(link.user_id):
                 link.counter += 1
                 session.commit()
-                return redirect(original_link)
+                return redirect(link.original.link)
             else:
                 return {'message': 'Недостаточно прав'}, 400
-    return {'message': 'Ссылка недействительна'}, 400
+    else:
+        return {'message': 'Ссылка недействительна'}, 400
 
 
 @jwt_required()
