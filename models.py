d0d7d2cd40e000efea25ae97719178b86a240232
mikhail matyukhin <79624065+matyuhin@users.noreply.github.com>
2021-05-26 20:26:47 +0500

    Update models.py

--- models.py
+++ models.py
@@ -3,9 +3,9 @@ from sqlalchemy.orm import relationship
 from flask_jwt_extended import create_access_token
 from datetime import timedelta
 from passlib.hash import pbkdf2_sha256
+#import hashlib
 
-
-
+#https://lectureswww.readthedocs.io/6.www.sync/2.codding/9.databases/2.sqlalchemy/
 class User(Base):
     __tablename__ = 'users'
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
@@ -34,29 +34,20 @@ class User(Base):
         return user
 
 
-class Link_type(Base):
-    __tablename__ = 'link_type'
+class Original(Base):
+    __tablename__ = 'original'
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-    type = db.Column(db.String(100), nullable=False, unique=True)
-    ink = relationship('Link', backref='link_type',
+    link = db.Column(db.String(300), nullable=False, unique=True)
+    ink = relationship('Link', backref='original',
                        lazy='dynamic')
+
 
 class Link(Base):
     __tablename__ = 'links'
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-    original = db.Column(db.String(500), nullable=False)
-    friendly_link = db.Column(db.String(500), nullable=True)
+    original_id = db.Column(db.Integer, db.ForeignKey('original.id'))
+    short_link = db.Column(db.String(200), nullable=False)
+    friendly_link = db.Column(db.String(100), nullable=True)
     user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
-    type_id = db.Column(db.Integer, db.ForeignKey('link_type.id'))
+    type_id = db.Column(db.Integer)
     counter = db.Column(db.Integer, nullable=True)
-    
-    
-    @classmethod
-    def get_original_link(cls, arg, type_query):
-        link = ()
-        if type_query == "id":
-            link = cls.query.filter(cls.id == arg).one()
-        else:
-            if cls.query.filter(cls.friendly_link == arg).count():
-                link = cls.query.filter(cls.friendly_link == arg).one()
-        return link
